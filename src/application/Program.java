0aaf465bda269f25be007e03fb69a1defa5ee64a
Bidfca <bruninhakjurugat11@gmail.com>
2020-05-20 13:50:38 -0300

    Some more impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,6 +18,17 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+		List<Seller> list = sellerDao.findAll();
+		
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
+		
+		Department department = new Department(2, null);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
