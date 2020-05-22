41844eb9a2f674152d126fed60e550db302dfc42
Bianca <bruninhakjurugat11@gmail.com>
2020-05-21 21:53:52 -0300

    and more impl

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -16,11 +17,9 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 		
 		List<Seller> list = sellerDao.findAll();
-		
 		for(Seller obj: list) {
 			System.out.println(obj);
 		}
@@ -29,6 +28,18 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		seller = sellerDao.findById(1);
+		seller.setName("Bianca Caetano");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
+		
 	}
 
 }
